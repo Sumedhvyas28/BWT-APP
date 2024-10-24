f0c3bd6b0612baeb597ef65660acd1c194b85832
sumedh vyas <sumedhvyas3@gmail.com>
2024-10-24 19:22:13 +0530

    api integration

---
+++ lib/constants/api_constant/routes/api_routes.dart
@@ -0,0 +1,4 @@
+class AppUrl {
+  static String mainDescription =
+      "https://dd35-45-113-107-90.ngrok-free.app/api/method/field_service_management.api.get_maintenance";
+}
---
+++ lib/data/network/NetworkApiService.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_application_1/data/app_exceptions.dart';
+import 'package:flutter_application_1/data/network/BaseApiService.dart';
+
+import 'package:http/http.dart' as http;
+
+class NetworkApiService extends BaseaApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          HttpHeaders.contentTypeHeader: 'application/json',
+        },
+      );
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response = await http
+          .post(
+            Uri.parse(url),
+            body: data,
+          )
+          .timeout(Duration(seconds: 10));
+
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 201:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 500:
+      case 404:
+        throw UnauthorisedException(response.body.toString());
+      default:
+        throw FetchDataException(
+            'Error accrued while communicating with server with status code${response.statusCode}');
+    }
+  }
+}
